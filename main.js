b2c94875865b811a888e2facbdf93f293312d4e4
SarahNoel <snturek@hotmail.com>
2015-07-20 20:16:47 -0600

    Fixed reset button

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+//deals the cards
 var showCards = document.getElementById("cards");
 
 showCards.onclick = function(){
   var cardContainer = document.getElementById('container');
   cardContainer.innerHTML = "";
   displayCards();
+  var resetAppear = document.getElementById('reset');
+  resetAppear.style.visibility = 'visible';
 };
 
+//run when Deal is clicked
 function displayCards(){
   var deck = newDeck();
   var shuffledCards = shuffleCards(deck);
@@ -20,9 +24,20 @@ function displayCards(){
   }
 }
 
+
+//Removes cards
+var resetCards = document.getElementById('reset');
+resetCards.onclick = function(){
+    var clearBoard = document.getElementById('container');
+  clearBoard.innerHTML = "";
+  var resetGone = document.getElementById('reset');
+  resetGone.style.visibility = 'hidden';
+};
+
 // Creates a deck of 52 cards
 function newDeck(){
-
+  // variables
+  var suits = ["d", "c", "s", "h"];
   var ranks = [
     {card:"a"},
     {card:"2"},
@@ -38,10 +53,28 @@ function newDeck(){
     {card:"q"},
     {card:"k"}
   ];
-
-  var suits = [ "d", "c", "s", "h"];
   var deck = [];
-}
+//for loops
+  for (i = 0; i < ranks.length; i++) {
+  for (j = 0; j < suits.length; j++) {
+    ranks[i].suit = suits[j];
+    deck.push({
+        card: ranks[i].card,
+        suit: ranks[i].suit});
+    }
+  }
+  return deck;
+};
 
 // Shuffles the Deck
-function shuffleCards(cardDeck){}
+function shuffleCards(cardDeck){
+ var result = [];
+ var cardDeckCopy = cardDeck.slice(0);
+ cardDeckLength = cardDeckCopy.length;
+ for (var i = 0; i < cardDeckLength; i++) {
+   var num = Math.floor(Math.random() * cardDeckCopy.length);
+   result.push(cardDeckCopy.splice(num, 1)[0]);
+ }
+ return result;
+}
+
